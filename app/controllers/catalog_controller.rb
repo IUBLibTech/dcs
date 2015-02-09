49c3d27576c72027549d2cd548b871514ea9fae8
Daniel Pierce <dlpierce@indiana.edu>
2015-02-09 18:05:05 -0500

    Show item_id in search results. Comment out spellcheck config option that was causing errors

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -100,6 +100,7 @@ class CatalogController < ApplicationController
     config.add_index_field 'published_display', :label => 'Published'
     config.add_index_field 'published_vern_display', :label => 'Published'
     config.add_index_field 'lc_callnum_display', :label => 'Call number'
+    config.add_index_field 'item_id', :label => 'Item ID'
 
 
     # solr fields to be displayed in the show (single result) view
@@ -146,7 +147,7 @@ class CatalogController < ApplicationController
 
     config.add_search_field('title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params.
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+      # field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
@@ -159,7 +160,7 @@ class CatalogController < ApplicationController
     end
 
     config.add_search_field('author') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+      # field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
       field.solr_local_parameters = {
         :qf => '$author_qf',
         :pf => '$author_pf'
@@ -170,12 +171,16 @@ class CatalogController < ApplicationController
     # tests can test it. In this case it's the same as
     # config[:default_solr_parameters][:qt], so isn't actually neccesary.
     config.add_search_field('subject') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+      # field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
       field.qt = 'search'
       field.solr_local_parameters = {
         :qf => '$subject_qf',
         :pf => '$subject_pf'
       }
+    end
+
+    config.add_search_field('item_id') do |field|
+
     end
 
     # "sort results by" select (pulldown)
